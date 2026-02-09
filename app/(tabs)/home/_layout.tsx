ab65ef071d4260b3fbf7b438197bd330e68b2f6c
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-10 01:37:31 +0530

    safe are navigate

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,6 +1,12 @@
-// app/(tabs)/home/_layout.tsx
 import { Stack } from "expo-router";
 
 export default function HomeLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: "#38208C" }, // 👈 add this
+      }}
+    />
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,12 @@ import { HistoryProvider } from "./context/HistoryContext";
 export default function RootLayout() {
   return (
     <HistoryProvider>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "#38208C" }, // 👈 add this
+        }}
+      >
         <Stack.Screen name="(tabs)" />
       </Stack>
     </HistoryProvider>
