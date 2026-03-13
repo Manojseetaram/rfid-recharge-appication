03abfb152a00da59dfc0b77c933e69a06f8e130f
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-03-13 19:42:10 +0530

    ..

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,48 @@
-import { Redirect } from 'expo-router';
+import { useEffect, useState } from "react";
+import { Redirect, useRouter } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
 
 export default function AppEntry() {
-  const isLoggedIn = false; 
+  const router = useRouter();
+  const [checking, setChecking] = useState(true);
+  const [logged, setLogged] = useState(false);
 
-  if (!isLoggedIn) {
-    return <Redirect href="/(auth)/login" />;
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = async () => {
+    const token = await SecureStore.getItemAsync("auth_token");
+    const isLoggedIn = await SecureStore.getItemAsync("is_logged_in");
+
+    if (token && isLoggedIn === "true") {
+      setLogged(true);
+    } else {
+      setLogged(false);
+    }
+
+    setChecking(false);
+  };
+
+  if (checking) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="#F2CB07" />
+      </View>
+    );
   }
+
+  if (!logged) return <Redirect href="/(auth)/login" />;
 
   return <Redirect href="/(tabs)/home" />;
 }
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    backgroundColor: "#0F0A2E",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
